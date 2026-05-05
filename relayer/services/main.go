57bb1f9d78a700bb24ad4681a94809d540da4a8c
neitdung <neitdung@gmail.com>
2026-05-06 01:08:29 +0700

    fix error build

--- relayer/services/main.go
+++ relayer/services/main.go
@@ -18,6 +18,7 @@ import (
 	clienttypes "github.com/cosmos/ibc-go/v10/modules/core/02-client/types"
 	channeltypesv2 "github.com/cosmos/ibc-go/v10/modules/core/04-channel/v2/types"
 	ibcexported "github.com/cosmos/ibc-go/v10/modules/core/exported"
+	ics23 "github.com/cosmos/ics23/go"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 )
@@ -475,7 +476,7 @@ func (s *Services) ethProofHeight(ctx Context, eventBlock uint64, sequence uint6
 	return ethClientState.LatestExecutionBlockNumber, ethClientState.LatestSlot, true
 }
 
-func parseMerkleProof(proofs [][]byte, sequence uint64) (tendermintContract.IMembershipMsgsMerkleProof, error) {
+func parseMerkleProof(proofs []*ics23.CommitmentProof, sequence uint64) (tendermintContract.IMembershipMsgsMerkleProof, error) {
 	merkleProof := tendermintContract.IMembershipMsgsMerkleProof{
 		Proofs: []tendermintContract.IMembershipMsgsCommitmentProof{},
 	}
