6975384f18e66770052f764db6b7fbfcde36d532
neitdung <neitdung@gmail.com>
2026-05-12 01:42:14 +0700

    chore: remove deprecated local test scripts for Ethereum and Cosmos nodes

--- e2e/local-tests/scripts/03-wasm.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec "$(cd "$(dirname "$0")/.." && pwd)/setup/03-wasm.sh" "$@"
--- e2e/local-tests/scripts/05-start-relayer.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec "$(cd "$(dirname "$0")/.." && pwd)/setup/05-relayer.sh" "$@"
