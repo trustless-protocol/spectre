84afe35b8d05d67adda58b4f23fdbf33f99b1cb4
neitdung <neitdung@gmail.com>
2026-05-12 11:53:41 +0700

    feat: update bucket sizes and enhance tests for SmallestBucketGEQ function

--- relayer/prover/prover_test.go
+++ relayer/prover/prover_test.go
@@ -46,7 +46,10 @@ func TestSmallestBucketGEQ(t *testing.T) {
 	}{
 		{1, 4, false},
 		{4, 4, false},
-		{5, 0, true},
+		{5, 8, false},
+		{17, 32, false},
+		{128, 128, false},
+		{129, 0, true},
 	}
 	for _, tc := range cases {
 		got, err := SmallestBucketGEQ(tc.in)
